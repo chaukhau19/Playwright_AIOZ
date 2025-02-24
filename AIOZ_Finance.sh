4c2875866a60128820fbc7aecf107292b49a2ca2
chaukhau19 <chaukhau2000@gmail.com>
2025-02-24 16:54:47 +0700

    update Jenkinsfile

--- AIOZ_Finance.sh
+++ AIOZ_Finance.sh
@@ -1,65 +1,29 @@
 #!/bin/bash
 
+# yarn test:ConnectMetaMask --workers=1
+# yarn test:DisconnectMetaMask --workers=1
+# yarn test:SwitchNetworkMetaMask --workers=1
 
-@echo off
-call yarn test:ConnectMetaMask --reporter=line --workers=1
-call yarn test:DisconnectMetaMask --reporter=line --workers=1
-call yarn test:SwitchNetworkMetaMask --reporter=line --workers=1
+# yarn test:ConnectCoinBase --workers=1
+# yarn test:DisconnectCoinBase --workers=1
+# yarn test:SwitchNetworkCoinBase --workers=1
 
-call yarn test:ConnectCoinBase --reporter=line --workers=1
-call yarn test:DisconnectCoinBase --reporter=line --workers=1
-call yarn test:SwitchNetworkCoinBase --reporter=line --workers=1
+# yarn test:AIOZtoUSDT_Valid --workers=1
+# yarn test:AIOZtoSTRK_Valid --workers=1
+# yarn test:STRKtoAIOZ_Valid --workers=1
+# yarn test:USDTtoAIOZ_Valid --workers=1
+# yarn test:AIOZtoWAIOZ_Valid --workers=1
 
-call yarn test:AIOZtoUSDT_Valid --reporter=line --workers=1
-call yarn test:AIOZtoSTRK_Valid --reporter=line --workers=1
-call yarn test:STRKtoAIOZ_Valid --reporter=line --workers=1
-call yarn test:USDTtoAIOZ_Valid --reporter=line --workers=1
-call yarn test:AIOZtoWAIOZ_Valid --reporter=line --workers=1
+# yarn test:AIOZtoSTRK_Invalid --workers=1
+# yarn test:AIOZtoUSDT_Invalid --workers=1
+# yarn test:STRKtoAIOZ_Invalid --workers=1
+# yarn test:USDTtoAIOZ_Invalid --workers=1
 
-call yarn test:AIOZtoSTRK_Invalid --reporter=line --workers=1
-pause
+# Uncomment these lines if needed
+# yarn test:AllFixed1 --workers=1
+# yarn test:AllFixed2 --workers=1
+# yarn test:AllFixed3 --workers=1
 
+yarn test:All --workers=1
 
-
-
-
-
-
-
-@REM set "REPORT_DIR=./playwright-report"
-
-@REM :: Tạo thư mục nếu chưa tồn tại
-@REM if not exist %REPORT_DIR% (
-@REM     mkdir %REPORT_DIR%
-@REM )
-
-@REM :: Chạy từng test với --workers=1 và lưu log riêng vào playwright-report
-@REM call yarn test:ConnectMetaMask --reporter=json --workers=1 >> %REPORT_DIR%/ConnectMetaMask.json
-@REM call yarn test:DisconnectMetaMask --reporter=json --workers=1 >> %REPORT_DIR%/DisconnectMetaMask.json
-
-@REM @REM call yarn test:SwitchNetworkMetaMask --reporter=json --workers=1 > %REPORT_DIR%/SwitchNetworkMetaMask.json
-
-@REM @REM call yarn test:ConnectCoinBase --reporter=json --workers=1 > %REPORT_DIR%/ConnectCoinBase.json
-@REM @REM call yarn test:DisconnectCoinBase --reporter=json --workers=1 > %REPORT_DIR%/DisconnectCoinBase.json
-@REM @REM call yarn test:SwitchNetworkCoinBase --reporter=json --workers=1 > %REPORT_DIR%/SwitchNetworkCoinBase.json
-
-@REM @REM call yarn test:AIOZtoUSDT_Valid --reporter=json --workers=1 > %REPORT_DIR%/AIOZtoUSDT_Valid.json
-@REM @REM call yarn test:AIOZtoSTRK_Valid --reporter=json --workers=1 > %REPORT_DIR%/AIOZtoSTRK_Valid.json
-@REM @REM call yarn test:STRKtoAIOZ_Valid --reporter=json --workers=1 > %REPORT_DIR%/STRKtoAIOZ_Valid.json
-@REM @REM call yarn test:USDTtoAIOZ_Valid --reporter=json --workers=1 > %REPORT_DIR%/USDTtoAIOZ_Valid.json
-@REM @REM call yarn test:AIOZtoWAIOZ_Valid --reporter=json --workers=1 > %REPORT_DIR%/AIOZtoWAIOZ_Valid.json
-
-
-
-@REM :: Gộp tất cả JSON thành 1 file (nếu có ít nhất 1 file JSON)
-@REM (for %%f in (%REPORT_DIR%\*.json) do type "%%f") > %REPORT_DIR%\Report.json
-
-@REM :: Chạy script JavaScript từ thư mục Libs
-@REM node Libs\convert_log_to_html.js
-
-@REM :: Kiểm tra nếu Log.html tồn tại trước khi mở
-@REM if exist %REPORT_DIR%\Log.html (
-@REM     start %REPORT_DIR%\Log.html
-@REM ) else (
-@REM     echo ❌ Lỗi: Không tạo được Log.html!
-@REM )
+read -p "Press Enter to continue..."
